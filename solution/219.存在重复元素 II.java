dfcf54fe109fcb8a2f4403a3f21e7d83c6be5cd1
LGZ <ligongzheng@alphesh.com>
2025-07-22 15:57:37 +0800

    new

---
+++ solution/219.存在重复元素 II.java
@@ -0,0 +1,32 @@
+/*
+ * @lc app=leetcode.cn id=219 lang=java
+ * @lcpr version=30201
+ *
+ * [219] 存在重复元素 II
+ */
+
+// @lc code=start
+class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        
+    }
+}
+// @lc code=end
+
+
+
+/*
+// @lcpr case=start
+// [1,2,3,1]\n3\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [1,0,1,1]\n1\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [1,2,3,1,2,3]\n2\n
+// @lcpr case=end
+
+ */
+
