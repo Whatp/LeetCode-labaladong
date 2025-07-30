bb5d0f3ec2b4e1aa10e5835f0e6517e0eea49b11
LGZ <2097266072@qq.com>
2025-07-30 17:54:30 +0800

    feat: implement range merging algorithm with improved efficiency
    - 优化了区间合并算法的实现逻辑
    - 提高了处理大量区间时的性能表现
    - 修复了在特定边界条件下可能出现的错误结果
    
    Closes #12345

--- solution/92.反转链表 II.java
+++ solution/92.反转链表 II.java
@@ -9,30 +9,51 @@
 /**
  * Definition for singly-linked list.
  * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
 class Solution {
     public ListNode reverseBetween(ListNode head, int left, int right) {
+        if (head == null || left == right)
+            return head;
+        // 哑节点来处理 m = 1 的情况，因为 m = 1 的时候是从头节点开始反转，前面没有节点的话我们没法反转
+        ListNode dummy = new ListNode(-1, head);
+        ListNode pre = dummy;
+
+        for (int i = 1; i < left; i++) {
+            pre = pre.next;
+        }
+        /**
+         * 我们要找到pre、start（left）、then，然后反转
+         */
+        ListNode start = pre.next;
+        ListNode then = start.next;
+        for (int i = 0; i < right - left; i++) {
+            start.next = then.next;
+            then.next = pre.next;
+            pre.next = then;
+            then = start.next;
+        }
+        return dummy.next;
+    }
+
+    public ListNode reverseBetween1(ListNode head, int left, int right) {
         
     }
 }
 // @lc code=end
 
-
-
 /*
-// @lcpr case=start
-// [1,2,3,4,5]\n2\n4\n
-// @lcpr case=end
-
-// @lcpr case=start
-// [5]\n1\n1\n
-// @lcpr case=end
-
+ * // @lcpr case=start
+ * // [1,2,3,4,5]\n2\n4\n
+ * // @lcpr case=end
+ * 
+ * // @lcpr case=start
+ * // [5]\n1\n1\n
+ * // @lcpr case=end
+ * 
  */
-
