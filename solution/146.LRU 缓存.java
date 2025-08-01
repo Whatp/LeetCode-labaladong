74031c7692b08301e98da7e6861314efc2838fcc
LGZ <2097266072@qq.com>
2025-08-01 11:01:48 +0800

    1

--- solution/146.LRU 缓存.java
+++ solution/146.LRU 缓存.java
@@ -6,18 +6,51 @@
  */
 
 // @lc code=start
+
+import java.util.LinkedHashMap;
+
 class LRUCache {
+    /**
+     * 哈希表查找快，但是没有固定顺序，链表有顺序之分，但是查找慢
+     * 结合二者，形成一种新的数据结构
+     * 
+     */
+    int cap;
+    LinkedHashMap<Integer, Integer> cache = new LinkedHashMap<>();
 
     public LRUCache(int capacity) {
-        
+        this.cap = capacity;
     }
-    
+
     public int get(int key) {
-        
+        if (!cache.containsKey(key)) {
+            return -1;
+        }
+        // 查找之后要把该元素变成最近使用过的
+        makeRecetly(key);
+        return cache.get(key);
+
     }
-    
-    public void put(int key, int value) {
-        
+
+    public void put(int key, int val) {
+        // 已经存在了，不需要判断size
+        if (cache.containsKey(key)) {
+            cache.put(key, val);
+            makeRecetly(key);
+            return;
+        }
+        if (cache.size() >= this.cap) {
+            int oldest = cache.keySet().iterator().next();
+            cache.remove(oldest);
+        }
+        cache.put(key, val);
+    }
+
+    private void makeRecetly(int key) {
+        // 从哈希链表中删除再添加
+        int val = cache.get(key);
+        cache.remove(key);
+        cache.put(key, val);
     }
 }
 
@@ -28,6 +61,3 @@ class LRUCache {
  * obj.put(key,value);
  */
 // @lc code=end
-
-
-
