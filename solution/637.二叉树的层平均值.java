ee4f98ce29f2dd438e6c60fa487336a8404e920d
LGZ <2097266072@qq.com>
2025-08-05 10:45:05 +0800

    2

---
+++ solution/637.二叉树的层平均值.java
@@ -0,0 +1,43 @@
+/*
+ * @lc app=leetcode.cn id=637 lang=java
+ * @lcpr version=30202
+ *
+ * [637] 二叉树的层平均值
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Double> averageOfLevels(TreeNode root) {
+        
+    }
+}
+// @lc code=end
+
+
+
+/*
+// @lcpr case=start
+// [3,9,20,null,null,15,7]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [3,9,20,15,7]\n
+// @lcpr case=end
+
+ */
+
