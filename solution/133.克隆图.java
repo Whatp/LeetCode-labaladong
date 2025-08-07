92c929423a4c2f7a9fdf21cde881dccf6a9faf4d
LGZ <2097266072@qq.com>
2025-08-07 09:32:51 +0800

    fix: 修正拼写错误

---
+++ solution/133.克隆图.java
@@ -0,0 +1,52 @@
+/*
+ * @lc app=leetcode.cn id=133 lang=java
+ * @lcpr version=30202
+ *
+ * [133] 克隆图
+ */
+
+// @lc code=start
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> neighbors;
+    public Node() {
+        val = 0;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val) {
+        val = _val;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val, ArrayList<Node> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+}
+*/
+
+class Solution {
+    public Node cloneGraph(Node node) {
+        
+    }
+}
+// @lc code=end
+
+
+
+/*
+// @lcpr case=start
+// [[2,4],[1,3],[2,4],[1,3]]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [[]]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// []\n
+// @lcpr case=end
+
+ */
+
