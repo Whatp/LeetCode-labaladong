7b8866e1a32029585598eae7d73f83bee08f5842
LGZ <2097266072@qq.com>
2025-08-08 10:02:36 +0800

    1

---
+++ solution/399.除法求值.java
@@ -0,0 +1,32 @@
+/*
+ * @lc app=leetcode.cn id=399 lang=java
+ * @lcpr version=30202
+ *
+ * [399] 除法求值
+ */
+
+// @lc code=start
+class Solution {
+    public double[] calcEquation(List<List<String>> equations, double[] values, List<List<String>> queries) {
+        
+    }
+}
+// @lc code=end
+
+
+
+/*
+// @lcpr case=start
+// [["a","b"],["b","c"]]\n[2.0,3.0]\n[["a","c"],["b","a"],["a","e"],["a","a"],["x","x"]]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [["a","b"],["b","c"],["bc","cd"]]\n[1.5,2.5,5.0]\n\n[["a","c"],["c","b"],["bc","cd"],["cd","bc"]]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [["a","b"]]\n[0.5]\n[["a","b"],["b","a"],["a","c"],["x","y"]]\n
+// @lcpr case=end
+
+ */
+
