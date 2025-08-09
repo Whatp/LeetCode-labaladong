eaf578bdabcc27201fc6bfb562ae77fa615ab0d2
LGZ <2097266072@qq.com>
2025-08-09 19:01:42 +0800

    1

---
+++ solution/433.最小基因变化.java
@@ -0,0 +1,32 @@
+/*
+ * @lc app=leetcode.cn id=433 lang=java
+ * @lcpr version=30202
+ *
+ * [433] 最小基因变化
+ */
+
+// @lc code=start
+class Solution {
+    public int minMutation(String startGene, String endGene, String[] bank) {
+        
+    }
+}
+// @lc code=end
+
+
+
+/*
+// @lcpr case=start
+// "AACCGGTT"\n"AACCGGTA"\n["AACCGGTA"]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// "AACCGGTT"\n"AAACGGTA"\n["AACCGGTA","AACCGCTA","AAACGGTA"]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// "AAAAACCC"\n"AACCCCCC"\n["AAAACCCC","AAACCCCC","AACCCCCC"]\n
+// @lcpr case=end
+
+ */
+
