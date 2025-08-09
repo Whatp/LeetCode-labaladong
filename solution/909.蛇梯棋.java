7a30eb6d9da42188c3265726568b1452e669ff42
LGZ <2097266072@qq.com>
2025-08-09 19:01:18 +0800

    1

---
+++ solution/909.蛇梯棋.java
@@ -0,0 +1,28 @@
+/*
+ * @lc app=leetcode.cn id=909 lang=java
+ * @lcpr version=30202
+ *
+ * [909] 蛇梯棋
+ */
+
+// @lc code=start
+class Solution {
+    public int snakesAndLadders(int[][] board) {
+        
+    }
+}
+// @lc code=end
+
+
+
+/*
+// @lcpr case=start
+// \n[[-1,-1,-1,-1,-1,-1],[-1,-1,-1,-1,-1,-1],[-1,-1,-1,-1,-1,-1],[-1,35,-1,-1,13,-1],[-1,-1,-1,-1,-1,-1],[-1,15,-1,-1,-1,-1]]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [[-1,-1],[-1,3]]\n
+// @lcpr case=end
+
+ */
+
