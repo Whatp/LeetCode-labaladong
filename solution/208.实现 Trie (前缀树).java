51e144d3a150417038646ef3b6d340af3d627474
LGZ <2097266072@qq.com>
2025-08-11 22:52:24 +0800

    前缀树解法

--- solution/208.实现 Trie (前缀树).java
+++ solution/208.实现 Trie (前缀树).java
@@ -7,36 +7,62 @@
 
 // @lc code=start
 
-import java.util.HashSet;
-
 class Trie {
 
-    HashSet<String> set;
+    private Trie[] children;
+    private boolean isEnd;
+
 
     public Trie() {
-        set = new HashSet<>();
+        children = new Trie[26];
+        isEnd = false;
     }
+
     
+    // [[a],[app],[apple]]  类似的数组
     public void insert(String word) {
-        set.add(word);
+        Trie node = this;
+        for (int i = 0; i < word.length(); i++) {
+            char ch = word.charAt(i);
+            int index = ch - 'a';
+            if (node.children[index] == null) {
+                node.children[index] = new Trie();
+            }
+            node = node.children[index];
+        }
+        node.isEnd = true;
     }
     
     public boolean search(String word) {
-        if (set.contains(word)) {
-            return true;
-        }
-        return false;
+        Trie node = searchPrefix(word);
+        return node != null && node.isEnd;
     }
     
     public boolean startsWith(String prefix) {
-        for (String string : set) {
-            if (string.startsWith(prefix)) {
-                return true;
+        return searchPrefix(prefix) != null;
+    }
+
+    public Trie searchPrefix(String prefix) {
+        Trie node = this;
+        for (int i = 0; i < prefix.length(); i++) {
+            char ch = prefix.charAt(i);
+            int index = ch - 'a';
+            if (node.children[index] == null) {
+                return null;
             }
+            node = node.children[index];
         }
-        return false;
+        return node;
     }
 }
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * Trie obj = new Trie();
+ * obj.insert(word);
+ * boolean param_2 = obj.search(word);
+ * boolean param_3 = obj.startsWith(prefix);
+ */
 
 /**
  * Your Trie object will be instantiated and called as such:
