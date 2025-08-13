9bec497e555cb395aca8e16161b49833146b55b1
LGZ <2097266072@qq.com>
2025-08-13 17:10:57 +0800

    4

--- solution/212.单词搜索 II.java
+++ solution/212.单词搜索 II.java
@@ -6,9 +6,53 @@
  */
 
 // @lc code=start
+
+import java.util.ArrayList;
+import java.util.List;
+
 class Solution {
     public List<String> findWords(char[][] board, String[] words) {
-        
+        List<String> res = new ArrayList<>();
+        for (String string : words) {
+            if (isExist(board, string)) {
+                res.add(string);
+            }
+        }
+        return res;
+    }
+
+    public boolean isExist(char[][] board, String words) {
+        int m = board.length, n = board[0].length;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (dfs(board, words, i, j, 0)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public boolean dfs(char[][] board, String words, int i, int j, int k) {
+        // base case
+        if (k == words.length()) return true;
+        if (i < 0 || i >= board.length || j < 0 || j >= board[0].length || board[i][j] != words.charAt(k)) {
+            return false;
+        }
+
+        // 记录并且标记已经访问了
+        char temp = board[i][j];
+        board[i][j] = '#';
+
+        boolean found = dfs(board, words, i + 1, j, k + 1) ||
+                        dfs(board, words, i - 1, j, k + 1) ||
+                        dfs(board, words, i, j + 1, k + 1) ||
+                        dfs(board, words, i , j - 1, k + 1);
+
+        // 恢复现场
+        board[i][j] = temp;
+        return found;
+
     }
 }
 // @lc code=end
