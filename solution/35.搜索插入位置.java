3908e88d5ab08854c5e416544ab4578f09fc5201
LGZ <2097266072@qq.com>
2025-08-15 09:40:20 +0800

    1

---
+++ solution/35.搜索插入位置.java
@@ -0,0 +1,32 @@
+/*
+ * @lc app=leetcode.cn id=35 lang=java
+ * @lcpr version=30202
+ *
+ * [35] 搜索插入位置
+ */
+
+// @lc code=start
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        
+    }
+}
+// @lc code=end
+
+
+
+/*
+// @lcpr case=start
+// [1,3,5,6]\n5\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [1,3,5,6]\n2\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [1,3,5,6]\n7\n
+// @lcpr case=end
+
+ */
+
