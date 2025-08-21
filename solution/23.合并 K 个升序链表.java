f9a7579e03a75864550ec5c2387c3adcae97c479
LGZ <2097266072@qq.com>
2025-08-21 10:50:18 +0800

    1

---
+++ solution/23.合并 K 个升序链表.java
@@ -0,0 +1,67 @@
+/*
+ * @lc app=leetcode.cn id=23 lang=java
+ * @lcpr version=30202
+ *
+ * [23] 合并 K 个升序链表
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        if (lists == null || lists.length == 0) return null;
+
+        ListNode l1 = null;
+        for (ListNode listNode : lists) {
+            l1 newNode = mergeTwo(l1, listNode);
+        }
+        return l1;
+    }
+
+    private ListNode mergeTwo(ListNode l1, ListNode l2) {
+        if (l1 == null) return l2;
+        if (l2 == null) return l1;
+        ListNode dummy = new ListNode(-1);
+        ListNode curr = dummy;
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                curr = l1;
+                l1 = l1.next;
+            } else {
+                curr = l2;
+                l2 = l2.next;
+            }
+            curr = curr.next;
+        }
+        curr.next = l1 == null ? l2 : l1;
+        return dummy.next;
+    }
+}
+// @lc code=end
+
+
+
+/*
+// @lcpr case=start
+// [[1,4,5],[1,3,4],[2,6]]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// []\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [[]]\n
+// @lcpr case=end
+
+ */
+
---
+++ solution/373.查找和最小的 K 对数字.java
@@ -0,0 +1,28 @@
+/*
+ * @lc app=leetcode.cn id=373 lang=java
+ * @lcpr version=30202
+ *
+ * [373] 查找和最小的 K 对数字
+ */
+
+// @lc code=start
+class Solution {
+    public List<List<Integer>> kSmallestPairs(int[] nums1, int[] nums2, int k) {
+        
+    }
+}
+// @lc code=end
+
+
+
+/*
+// @lcpr case=start
+// [1,7,11]\n[2,4,6]\n3\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [1,1,2]\n[1,2,3]\n2\n
+// @lcpr case=end
+
+ */
+
