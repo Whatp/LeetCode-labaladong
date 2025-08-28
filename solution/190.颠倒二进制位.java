8692544edc66b38203c23656d82e9e02d96c132c
LGZ <2097266072@qq.com>
2025-08-28 15:00:50 +0800

    a

---
+++ solution/190.颠倒二进制位.java
@@ -0,0 +1,28 @@
+/*
+ * @lc app=leetcode.cn id=190 lang=java
+ * @lcpr version=30202
+ *
+ * [190] 颠倒二进制位
+ */
+
+// @lc code=start
+class Solution {
+    public int reverseBits(int n) {
+        
+    }
+}
+// @lc code=end
+
+
+
+/*
+// @lcpr case=start
+// 43261596\n
+// @lcpr case=end
+
+// @lcpr case=start
+// 2147483644\n
+// @lcpr case=end
+
+ */
+
