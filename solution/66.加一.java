81f7256eb38a4dc5d3ceaa8a3930688444f35a66
LGZ <2097266072@qq.com>
2025-08-30 21:57:02 +0800

    5

---
+++ solution/66.加一.java
@@ -0,0 +1,32 @@
+/*
+ * @lc app=leetcode.cn id=66 lang=java
+ * @lcpr version=30202
+ *
+ * [66] 加一
+ */
+
+// @lc code=start
+class Solution {
+    public int[] plusOne(int[] digits) {
+        
+    }
+}
+// @lc code=end
+
+
+
+/*
+// @lcpr case=start
+// [1,2,3]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [4,3,2,1]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [9]\n
+// @lcpr case=end
+
+ */
+
