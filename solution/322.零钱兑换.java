1553d624ed18ab138b0124d2ee5918d60cb510cd
LGZ <2097266072@qq.com>
2025-09-04 21:19:39 +0800

    322

--- solution/322.零钱兑换.java
+++ solution/322.零钱兑换.java
@@ -10,30 +10,29 @@
 import java.util.Arrays;
 
 class Solution {
-    int moemo[];
+
+    /**
+     * 考虑自底向上的解法
+     * dp[i] 的定义是什么，凑金额 i 最少需要dp[i]枚硬币
+     * @param coins
+     * @param amount
+     * @return
+     */
     public int coinChange(int[] coins, int amount) {
-        moemo = new int[amount + 1];
-        Arrays.fill(moemo, -66);
-
-        return dp(coins, amount);
-
-    }
-
-    int dp(int[] coins, int amount) {
-        if (amount < 0) return -1;
         if (amount == 0) return 0;
+        int[] dp = new int[amount + 1];
+        Arrays.fill(dp, amount + 1);
+        dp[0] = 0;
 
-        if (moemo[amount] != -66) return moemo[amount];
-
-        int res = Integer.MAX_VALUE;
-        for (int coin : coins) {
-            int subProblem = dp(coins, amount - coin);
-            if (subProblem == -1) continue;
-            res = Math.min(res, subProblem + 1);
+        for (int i = 0; i < amount + 1; i++) {
+            for (int coin : coins) {
+                if (i < coin) continue;
+                dp[i] = Math.min(dp[i], 1 + dp[i - coin]);
+            }
         }
-        moemo[amount] = (res == Integer.MAX_VALUE) ? -1 : res;
-        return moemo[amount];
+        return dp[amount] == amount + 1 ? -1 : dp[amount];
     }
+
 }
 // @lc code=end
 
