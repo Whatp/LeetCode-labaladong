af062853dd8dd954ae6e24ed936095a1d38541ce
LGZ <2097266072@qq.com>
2025-09-06 22:39:03 +0800

    4

--- solution/300.最长递增子序列.java
+++ solution/300.最长递增子序列.java
@@ -11,20 +11,24 @@ import java.util.Arrays;
 
 class Solution {
     public int lengthOfLIS(int[] nums) {
-        // dp[i] 表示 nums[i] 这个数字结尾的最长递归子序列长度
+        /**
+         * dp[i] 代表以 i 结尾的最长递增子序列的长度，选择当前的或者不选
+         */
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
         int[] dp = new int[nums.length];
-        Arrays.fill(dp, 1);
-        for (int i = 0; i < nums.length; i++) {
+        dp[0] = 1;
+        int res = 1;
+
+        for (int i = 1; i < nums.length; i++) {
+            dp[i] = 1;
             for (int j = 0; j < i; j++) {
                 if (nums[i] > nums[j]) {
                     dp[i] = Math.max(dp[i], dp[j] + 1);
                 }
             }
-        }
-
-        int res = 0;
-        for (int i : dp) {
-            res = Math.max(res, i);
+            res = Math.max(res, dp[i]);
         }
         return res;
     }
